909bebdbdc507b3d60f86773c3e85dc657898147
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-11-26 10:28:52 -0600

    Agrega ejemplo de plantilla básica con PHP

---
+++ PHP/plantilla.php
@@ -0,0 +1,22 @@
+<?php
+$nombre = "Jaime Delgado";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Plantilla PHP</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+</head>
+
+<body>
+  <main class="container">
+    <article>
+      <p>Hola me llamo <?= $nombre ?></p>
+    </article>
+  </main>
+</body>
+
+</html>
